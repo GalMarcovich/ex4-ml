60b9f365cad3a64e2ddea9b5eedd348e648e8519
Gal Marcovich <gal.marcovich@live.biu.ac.il>
2019-06-18 12:45:21 +0300

    4 commit

--- data_loader_tester.py
+++ data_loader_tester.py
@@ -6,6 +6,6 @@ dataset = GCommandLoader('./gcommands/train')
 test_loader = torch.utils.data.DataLoader(
         dataset, batch_size=100, shuffle=None,
         num_workers=20, pin_memory=True, sampler=None)
-#noam
+
 for k, (input,label) in enumerate(test_loader):
     print(input.size(), len(label))
